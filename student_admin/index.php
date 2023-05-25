7c5ba3a620379d96d2577674c4c5a2776fb9e3cd
RIjvan-Juneja <junejarijvan8128@gmail.com>
2023-05-25 19:40:05 +0530

    student_admin
    - login completed

--- student_admin/index.php
+++ student_admin/index.php
@@ -1,112 +1,140 @@
+<?php
+include "../Database/connect.php";
+include "../common/function.php";
+
+session_start();
+$login_status_error="";
+// Check if the user is already logged in
+if (isset($_SESSION['username'])) {
+  // User is already logged in, redirect to the home page or dashboard
+  header('Location: student_admin\index.php');
+  exit();
+}
+
+// Check if the form is submitted
+if (isset($_POST['sign_in'])) {
+
+  // Get the submitted username and password
+  $username = mysqli_real_escape_string($con,$_POST['email_username']);
+  $password = mysqli_real_escape_string($con,$_POST['password']);
+
+  // Prepare and execute the SQL statement
+  $cmd = $con->prepare("SELECT id as user_id, username,password FROM student_admin WHERE username = ? AND password = ? ");
+  $cmd->bind_param("ss", $username,$password);
+  $cmd->execute();
+  $ex = $cmd->get_result();
+
+  if ($ex->num_rows == 1) {
+
+    $row = mysqli_fetch_array($ex);
+
+    $user_id = $row['user_id'];
+    $_SESSION['user_id'] = $user_id;
+    $login_status = "success";
+
+    header("Location: http://localhost/GOsP/student_admin/html/index.php");
+
+  }else{
+    $login_status = "failed";
+  }
+
+}
+  
+?>
+
+
 <!DOCTYPE html>
 
-<html
-  lang="en"
-  class="light-style customizer-hide"
->
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
+<html lang="en" class="light-style customizer-hide">
 
-    <title>Admin Login</title>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <meta name="description" content="" />
+  <title>Admin Login</title>
 
-    <!-- Favicon -->
-    <!-- <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" /> -->
+  <meta name="description" content="" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
+  <!-- Favicon -->
+  <!-- <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" /> -->
 
-    <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet" />
 
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+  <!-- Icons. Uncomment required icon fonts -->
+  <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
 
-    <!-- Page -->
-    <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
-    <link rel="stylesheet" href="../componants/header/header.css">
-    <link rel="stylesheet" href="../componants/footer/footer.css">
+  <!-- Core CSS -->
+  <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+
+  <!-- Page -->
+  <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
+  <link rel="stylesheet" href="../componants/header/header.css">
+  <link rel="stylesheet" href="../componants/footer/footer.css">
 
 
-  </head>
+</head>
 
-  <body>
-    <!-- header  -->
+<body>
+  <!-- header  -->
   <?php include "../componants/header/header.php"; ?>
 
-    <!-- Content -->
-    
-    <div class="container-xxl">
-      <div class="authentication-wrapper authentication-basic container-p-y">
-        <div class="authentication-inner">
-          <!-- Register -->
-          <div class="card">
-            <div class="card-body">
-              <!-- Logo -->
-              <div class="app-brand justify-content-center">
-                  <span class="app-brand-text demo text-body fw-bolder" style="font-size: 31px;">GOsP</span>
-              </div>
-              <!-- /Logo -->
-              <h4 class="mb-10">Welcome to Student Admin! 👋</h4>
-              <form id="formAuthentication" class="mb-3 mt-sm-3" action="html/index.php" method="POST">
-                <div class="mb-3">
-                  <label for="email" class="form-label">Username</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="email"
-                    name="email-username"
-                    placeholder="Enter your email or username"
-                    autofocus
-                    required
-                  />
-                </div>
-                <div class="mb-3 form-password-toggle">
-                  <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Password</label>
-                    <a href="auth-forgot-password-basic.html">
-                      <small>Forgot Password?</small>
-                    </a>
-                  </div>
-                  <div class="input-group input-group-merge">
-                    <input
-                      type="password"
-                      id="password"
-                      class="form-control"
-                      name="password"
-                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                      aria-describedby="password"
-                      required
-                    />
-                   
-                  </div>
-                </div>
-                
-                <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                  <!-- <a href="html/index.php" class="btn btn-primary d-grid w-100">Sign in</a> -->
-                </div>
-              </form>
+  <!-- Content -->
+
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <span class="app-brand-text demo text-body fw-bolder" style="font-size: 31px;">GOsP</span>
             </div>
+            <!-- /Logo -->
+            <h4 class="mb-10">Welcome to Student Admin! 👋</h4>
+            <form id="formAuthentication" action="index.php" class="mb-3 mt-sm-3" method="POST">
+              <div class="mb-3">
+                <label for="email" class="form-label">Username</label>
+                <input type="text" class="form-control" id="email" name="email_username"
+                  placeholder="Enter your email or username" autofocus required />
+              </div>
+              <div class="mb-3 form-password-toggle">
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">Password</label>
+                  <a href="auth-forgot-password-basic.html">
+                    <small>Forgot Password?</small>
+                  </a>
+                </div>
+                <div class="input-group input-group-merge">
+                  <input type="password" id="password" class="form-control" name="password"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" required />
+
+                </div>
+                <div class="form-text color-red"><?php echo $login_status_error; ?></div>
+              </div>
+
+              <div class="mb-3">
+                <button name="sign_in" class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                <!-- <a href="html/index.php" class="btn btn-primary d-grid w-100">Sign in</a> -->
+              </div>
+            </form>
           </div>
-          <!-- /Register -->
         </div>
+        <!-- /Register -->
       </div>
     </div>
+  </div>
 
 
-    <!-- footer -->
-    <?php include "../componants/footer/footer.php"; ?>
-  </body>
+  <!-- footer -->
+  <?php include "../componants/footer/footer.php"; ?>
+</body>
+
 </html>
