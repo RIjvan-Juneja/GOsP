f7b6da7bc902bf0e539f2d0ca2fd0095c01d10bc
RIjvan-Juneja <junejarijvan8128@gmail.com>
2023-06-06 13:25:18 +0530

    website

--- detail_profile.php
+++ detail_profile.php
@@ -8,180 +8,15 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Exo:400,700');
-
-*{
-    margin: 0px;
-    padding: 0px;
-}
-
-body{
-    font-family: 'Exo', sans-serif;
-    color: #fff;
-}
-
-
-.context {
-    width: 100%;
-    position: absolute;
-    top:7vh;
-    
-}
-
-.context h1{
-    text-align: center;
-    color: #fff;
-    font-size: 50px;
-}
-
-
-.area{
-    background: #4e54c8;  
-    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);  
-    width: 100%;
-    height:100vh;
-    
-   
-}
-
-.circles{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-}
-
-.circles li{
-    position: absolute;
-    display: block;
-    list-style: none;
-    width: 20px;
-    height: 20px;
-    background: rgba(255, 255, 255, 0.2);
-    animation: animate 25s linear infinite;
-    bottom: -150px;
-    
-}
-
-.circles li:nth-child(1){
-    left: 25%;
-    width: 80px;
-    height: 80px;
-    animation-delay: 0s;
-}
-
-
-.circles li:nth-child(2){
-    left: 10%;
-    width: 20px;
-    height: 20px;
-    animation-delay: 2s;
-    animation-duration: 12s;
-}
-
-.circles li:nth-child(3){
-    left: 70%;
-    width: 20px;
-    height: 20px;
-    animation-delay: 4s;
-}
-
-.circles li:nth-child(4){
-    left: 40%;
-    width: 60px;
-    height: 60px;
-    animation-delay: 0s;
-    animation-duration: 18s;
-}
-
-.circles li:nth-child(5){
-    left: 65%;
-    width: 20px;
-    height: 20px;
-    animation-delay: 0s;
-}
-
-.circles li:nth-child(6){
-    left: 75%;
-    width: 110px;
-    height: 110px;
-    animation-delay: 3s;
-}
-
-.circles li:nth-child(7){
-    left: 35%;
-    width: 150px;
-    height: 150px;
-    animation-delay: 7s;
-}
-
-.circles li:nth-child(8){
-    left: 50%;
-    width: 25px;
-    height: 25px;
-    animation-delay: 15s;
-    animation-duration: 45s;
-}
-
-.circles li:nth-child(9){
-    left: 20%;
-    width: 15px;
-    height: 15px;
-    animation-delay: 2s;
-    animation-duration: 35s;
-}
-
-.circles li:nth-child(10){
-    left: 85%;
-    width: 150px;
-    height: 150px;
-    animation-delay: 0s;
-    animation-duration: 11s;
-}
-
-
-
-@keyframes animate {
-
-    0%{
-        transform: translateY(0) rotate(0deg);
-        opacity: 1;
-        border-radius: 0;
-    }
-
-    100%{
-        transform: translateY(-1000px) rotate(720deg);
-        opacity: 0;
-        border-radius: 50%;
-    }
-
-}
-        
-
-        tr:nth-child(even) {
-            background: #ff4c4c21;
-        }
-
-        
-
-        td,
-        td b {
-            text-transform: uppercase;
-        }
-        .mb{
-            margin-bottom: 20px;
-        }
-    </style>
+    <link rel="stylesheet" href="detail_profile.css">
 </head>
 
 <body>
     <div class="context">
         <div class="container">
             <h2 class="text-center font-bold mb">User Application</h2>
-            <table class="table table-bordered">
+
+            <table class="table table-bordered table1">
                 <tbody>
                     <tr>
                         <td><b>Name</b></td>
@@ -209,7 +44,7 @@ body{
                         <td><b>Skills :</b></td>
                         <td colspan="5">HTML,css,jS</td>
                     </tr>
-                    
+
 
                     <tr>
                         <td colspan="5">
@@ -224,23 +59,84 @@ body{
 
                 </tbody>
             </table>
+
+            <table class="table table-bordered table2">
+                <tbody>
+                    <tr>
+                        <td width="100px" rowspan="3"><img src="assets/images/user2.jpg" alt="" width="100px"></td>
+                    </tr>
+                    <tr>
+                        <td><b>Name :</b> <br>
+                            JUNEJA RIJVAN
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><b>Enrollment :</b> <br>
+                            2012901016020
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>Course :</b>
+                            B.E
+                        </td>
+                        <td>
+                            <b>Branch:</b>
+                            IT
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>Semester :</b>
+                            6
+                        </td>
+                        <td>
+                            <b>GitHub:</b>
+                            <a href="">link</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <b>Email :</b>
+                            avbjd@gmail.com
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <b>skills :</b> <br>
+                            ffd,ddhd,djhfhf,ssjhf
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <b>About us :</b> <br>
+                            <p class="text-justify"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                                provident, maiores eveniet, itaque omnis corrupti, quam id accusamus quo harum facere!
+                                Molestiae aspernatur minima consequuntur nemo qui voluptate cumque hic eaque dicta,
+                                maxime porro! Laudantium doloribus unde ab, labore mollitia excepturi, facere sunt enim
+                                soluta nam saepe natus! Dicta, itaque.</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
     </div>
-    <div class="area" >
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
+    <div class="area">
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
 
 </body>
 
