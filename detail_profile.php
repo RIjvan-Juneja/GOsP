09e84702c2353680050a6028d8f3e9b6537e2461
Jainam1303 <128685594+Jainam1303@users.noreply.github.com>
2023-06-23 17:24:55 +0530

    Update detail_profile.php
    detaile.php

--- detail_profile.php
+++ detail_profile.php
@@ -15,6 +15,7 @@ $stmt = $con->prepare("SELECT * FROM `user` where enrollment_number =?");
 		$last_name = !empty($row['last_name']) ? $row['last_name'] : "";
 		$er_num = !empty($row['enrollment_number']) ? $row['enrollment_number'] : "";
 		$email = !empty($row['email']) ? $row['email'] : "";
+        $cource = !empty($row['cource']) ? $row['cource'] : "";
 		$branch = !empty($row['branch']) ? $row['branch'] : "";
 		$semester = !empty($row['semester']) ? $row['semester'] : "";
 		$skills = !empty($row['skills']) ? $row['skills'] : "";
@@ -48,40 +49,44 @@ $stmt = $con->prepare("SELECT * FROM `user` where enrollment_number =?");
                 <tbody>
                     <tr>
                         <td><b>Name</b></td>
-                        <td><?php echo $first_name; ?></td>
+                        <td><?php echo $first_name . " " . $middle_name . " " . $last_name; ?></td>
                         <td><b>Enrollment Number </b></td>
-                        <td>201290116020</td>
+                        <td><?php echo $er_num; ?></td>
                         <td rowspan="4" width="100px"><img src="assets/images/user2.jpg" alt="" width="180px"></td>
                     </tr>
                     <tr>
                         <td><b>Course</b></td>
-                        <td>B.E</td>
+                        <td><?php echo $cource; ?></td>
                         <td><b>Branch</b></td>
-                        <td>IT</td>
+                        <td><?php echo $branch; ?></td>
                     </tr>
                     <tr>
                         <td><b>Semester</b></td>
-                        <td>3</td>
+                        <td><?php echo $semester; ?></td>
                         <td><b>Email Id</b></td>
-                        <td>asdd54@bdh.com</td>
+                        <td><?php echo $email; ?></td>
                     </tr>
                     <tr>
-                        <td colspan="4"> <b>GitHub Link : </b><a href="">link</a></td>
+                        <td colspan="4"> <b>GitHub Link : </b><a href=""><?php echo $github_link; ?></a></td>
                     </tr>
                     <tr>
                         <td><b>Skills :</b></td>
-                        <td colspan="5">HTML,css,jS</td>
+                        <td colspan="5"> <?php $array = explode(',', $skills); 
+                        foreach ($array as $key => $value) {
+                            if ($value === end($array)) {
+                                echo " " . $value;
+                            } else {
+                                echo $value . ", ";
+                            }
+                        }
+                            ?></td>
                     </tr>
 
 
                     <tr>
                         <td colspan="5">
                             <b>About Us :</b> <br><br>
-                            <p class="text-justify"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-                                provident, maiores eveniet, itaque omnis corrupti, quam id accusamus quo harum facere!
-                                Molestiae aspernatur minima consequuntur nemo qui voluptate cumque hic eaque dicta,
-                                maxime porro! Laudantium doloribus unde ab, labore mollitia excepturi, facere sunt enim
-                                soluta nam saepe natus! Dicta, itaque.</p>
+                            <p class="text-justify"> <?php echo $description; ?></p>
                         </td>
                     </tr>
 
@@ -95,55 +100,59 @@ $stmt = $con->prepare("SELECT * FROM `user` where enrollment_number =?");
                     </tr>
                     <tr>
                         <td><b>Name :</b> <br>
-                            JUNEJA RIJVAN
+                        <?php echo $first_name . " " . $middle_name . " " . $last_name; ?>
                         </td>
                     </tr>
                     <tr>
                         <td><b>Enrollment :</b> <br>
-                            2012901016020
+                        <?php echo $er_num; ?>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <b>Course :</b>
-                            B.E
+                            <?php echo $cource; ?>
                         </td>
                         <td>
                             <b>Branch:</b>
-                            IT
+                            <?php echo $branch; ?>
                         </td>
 
                     </tr>
                     <tr>
                         <td>
                             <b>Semester :</b>
-                            6
+                            <?php echo $semester; ?>
                         </td>
                         <td>
                             <b>GitHub:</b>
-                            <a href="">link</a>
+                            <a href=""><?php echo $github_link; ?></a>
                         </td>
                     </tr>
                     <tr>
                         <td colspan="2">
                             <b>Email :</b>
-                            avbjd@gmail.com
+                            <?php echo $email; ?>
                         </td>
                     </tr>
                     <tr>
                         <td colspan="2">
                             <b>skills :</b> <br>
-                            ffd,ddhd,djhfhf,ssjhf
+                            <td colspan="5"> <?php $array = explode(',', $skills); 
+                        foreach ($array as $key => $value) {
+                            if ($value === end($array)) {
+                                echo " " . $value;
+                            } else {
+                                echo $value . ", ";
+                            }
+                        }
+                            ?>
                         </td>
                     </tr>
                     <tr>
                         <td colspan="2">
                             <b>About us :</b> <br>
-                            <p class="text-justify"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-                                provident, maiores eveniet, itaque omnis corrupti, quam id accusamus quo harum facere!
-                                Molestiae aspernatur minima consequuntur nemo qui voluptate cumque hic eaque dicta,
-                                maxime porro! Laudantium doloribus unde ab, labore mollitia excepturi, facere sunt enim
-                                soluta nam saepe natus! Dicta, itaque.</p>
+                            <p class="text-justify"><?php echo $description; ?></p>
                         </td>
                     </tr>
                 </tbody>
